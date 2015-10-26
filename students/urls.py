c3ba144acdec88eb4e9c8a04f4dd28b936ef78b8
dmitry.gogenko <gogenko@ukr.net>
2015-10-26 17:37:29 +0200

    add tags {% url %} into template

---
+++ students/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, patterns, include
+from django.contrib import admin
+import views
+
+urlpatterns = [
+    # url(r'^$', 'views.students_list', name='home'),
+    # url(r'^groups/$', 'views.groups_list', name='groups'),
+]
